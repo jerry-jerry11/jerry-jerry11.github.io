9acf9b0b877444d22c954e2c85d486fbef5ec36b
jerry-jerry11 <104097025+jerry-jerry11@users.noreply.github.com>
2022-06-21 16:38:01 +0200

    added files

---
+++ order.vb
@@ -0,0 +1,9 @@
+'declaring allvariables
+dim intPizaflavor as integer=0
+dim intExtra as integer = 0
+dim strCustomername as string = ""
+dim Customersurname as string ""
+dim decPrices as decimal = 0D
+
+'POPULATE THE COMBOBOX
+cmbPizzaflavor.items.add()
---
+++ x = 1.py
@@ -0,0 +1,17 @@
+x = 1
+if x == 1:
+    print("x is 1")
+
+#NUMBERS
+myInt = 7
+print(myInt)
+
+##float
+myFloat = 0.7
+print(myFloat)
+myFloat = float(7)
+print(myFloat)
+
+##STRING
+myString = 'hello'
+print(myString)
